feb4183718edc745a1f5d3729d6a167622dada2c
James Woolley-Ritchie <jameswoolleyritchie@gmail.com>
2024-12-21 21:14:33 +0000

    Day 13, holy accounting for rounding errors and finding the craziest coincidence that threw the answer off Batman!

--- Day_13/Part_1.java
+++ Day_13/Part_1.java
@@ -1,27 +1,50 @@
 package Day_13;
 
-public class Part_1 {
-    public static void main(String[] args) {
-        // Try decrementing numbers of B presses until you get a solution unless there isn't one (e.g. all the way from all B to all A doesn't work)
-        int a_x = 94;
-        int a_y = 34;
-        int b_x = 22;
-        int b_y = 67;
-        int prize_x = 8400;
-        int prize_y = 5400;
-        
-        // Find the max number of B presses that fit into the prize x and y.
-        int max_number_of_b = Math.min((int) Math.floor(prize_x / Math.min(a_x, b_x)), (int) Math.floor(prize_y / Math.min(a_y, b_y)));
-        int actual_number_of_a = -1;
-        int actual_number_of_b = -1;
-        for (int i = max_number_of_b; i >= 0 ; i--) {
-            if (prize_x == (b_x * i) + (a_x * (max_number_of_b - i)) && prize_y == b_y * max_number_of_b + (a_y * (max_number_of_b - i))) {
-                actual_number_of_a = max_number_of_b - i;
-                actual_number_of_b = i;
-            }
-        }
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-        int number_of_tokens = (actual_number_of_a * 3) + actual_number_of_b;
-        System.out.println(number_of_tokens);
+public class Part_1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new File(".\\Day_13\\input.txt"))) {
+            int a_x = 0;
+            int a_y = 0;
+            int b_x = 0;
+            int b_y = 0;
+            int prize_x = 0;
+            int prize_y = 0;
+            
+            int total_tokens = 0;
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                if (line.contains("A")) {
+                    a_x = Integer.parseInt(line.split(",")[0].replace("Button A: X", ""));
+                    a_y = Integer.parseInt(line.split(",")[1].replace(" Y", ""));
+                } else if (line.contains("B")) {
+                    b_x = Integer.parseInt(line.split(",")[0].replace("Button B: X", ""));
+                    b_y = Integer.parseInt(line.split(",")[1].replace(" Y", ""));
+                } else if (line.contains("Prize")) {
+                    prize_x = Integer.parseInt(line.split(",")[0].replace("Prize: X=", ""));
+                    prize_y = Integer.parseInt(line.split(",")[1].replace(" Y=", ""));
+                } else {
+                    // Try decrementing numbers of B presses until you get a solution unless there isn't one (e.g. all the way from all B to all A doesn't work)
+                    // Find the max number of B presses that fit into the prize x and y.
+                    int max_number_of_b = Math.min((int) Math.floor(prize_x / b_x), (int) Math.floor(prize_y / b_y));
+                    int actual_number_of_a = 0;
+                    int actual_number_of_b = 0;
+                    for (int i = max_number_of_b; i >= 0 ; i--) {
+                        if ((prize_x - (i * b_x)) % a_x == 0 && (prize_y - (i * b_y)) % a_y == 0) {
+                            if ((prize_x - (i * b_x)) / a_x == (prize_y - (i * b_y)) / a_y) {
+                                actual_number_of_a = (prize_x - (i * b_x)) / a_x;
+                                actual_number_of_b = i;
+                            }
+                        }
+                    }
+
+                    total_tokens += (actual_number_of_a * 3) + actual_number_of_b;
+                }
+            }
+            System.out.println(total_tokens);
+        }
     }
 }
--- Day_13/Part_2.java
+++ Day_13/Part_2.java
@@ -1,5 +1,42 @@
 package Day_13;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part_2 {
-    
+    public static void main(String[] args) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new File(".\\Day_13\\input.txt"))) {
+            long a_x = 0L;
+            long a_y = 0L;
+            long b_x = 0L;
+            long b_y = 0L;
+            long prize_x;
+            long prize_y;
+            
+            long total_tokens = 0;
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                if (line.contains("A")) {
+                    a_x = Long.parseLong(line.split(",")[0].replace("Button A: X", ""));
+                    a_y = Long.parseLong(line.split(",")[1].replace(" Y", ""));
+                } else if (line.contains("B")) {
+                    b_x = Long.parseLong(line.split(",")[0].replace("Button B: X", ""));
+                    b_y = Long.parseLong(line.split(",")[1].replace(" Y", ""));
+                } else if (line.contains("Prize")) {
+                    prize_x = 10000000000000L + Long.parseLong(line.split(",")[0].replace("Prize: X=", ""));
+                    prize_y = 10000000000000L + Long.parseLong(line.split(",")[1].replace(" Y=", ""));
+
+                    // Try and do it the maths way.
+                    double number_of_b = (prize_y - (prize_x * (a_y / (double) a_x))) / (double) (b_y - (b_x * (a_y / (double) a_x)));
+                    double number_of_a = (prize_x - (number_of_b * b_x)) / (double) a_x;
+
+                    if ((Math.round(number_of_a) * a_x) + (Math.round(number_of_b) * b_x) == prize_x && (Math.round(number_of_a) * a_y) + (Math.round(number_of_b) * b_y) == prize_y) { 
+                        total_tokens += number_of_b + (number_of_a * 3);
+                    }
+                }
+            }
+            System.out.println(total_tokens);
+        }
+    }
 }
